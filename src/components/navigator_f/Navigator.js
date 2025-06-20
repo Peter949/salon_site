498aef743f69ce511c7a1ebb2debace10d16d50c
Your Name <@youexample.com>
2025-06-20 23:12:37 +0900

    doing a katalog

--- src/components/navigator_f/Navigator.js
+++ src/components/navigator_f/Navigator.js
@@ -1,26 +1,39 @@
 import Button from '../button_f/Button';
 import styles from './Navigator.module.css'
+import { useState } from 'react';
 
 const Navigator = () =>
 {
-    function click(page_name)
-    {
-
-        alert(page_name);
-    };
     const btn_name_arr = ["Магазин", "Парикмахеры", "Запись на прием", "Все прически", "Личный кабинет"];
+    const [btn_state, setBtnState] = useState(['Магазин']);
 
-    // var btn_arr = [<Button text={btn_name_arr[0]} onClick={click}/>,
-    //         <Button text={btn_name_arr[1]} onClick={click}/>,
-    //         <Button text={btn_name_arr[2]} onClick={click} />,
-    //         <Button text={btn_name_arr[3]} onClick={click}/>,
-    //         <Button text={btn_name_arr[4]} onClick={click}/>];
+    const btn_styles = [styles.btn_style, styles.btn_style, styles.btn_style, styles.btn_style, styles.btn_style];
 
-    const btn_arr = btn_name_arr.map((name) => (
+    switch(btn_state)
+    {
+        case btn_name_arr[0]:
+            btn_styles[0] = styles.active_btn_style;
+            break;
+        case btn_name_arr[1]:
+            btn_styles[1] = styles.active_btn_style;
+            break;
+        case btn_name_arr[2]:
+            btn_styles[2] = styles.active_btn_style;
+            break;
+        case btn_name_arr[3]:
+            btn_styles[3] = styles.active_btn_style;
+            break;
+        case btn_name_arr[4]:
+            btn_styles[4] = styles.active_btn_style;
+            break;
+    }
+    
+    const btn_arr = btn_name_arr.map((name, index) => (
         <Button 
-            //key={name}
+            //key={name} 
             text={name} 
-            onClick={() => click(name)}
+            add_style = {btn_styles[index]}
+            onClick={() => setBtnState(name)}
         />
     ));
 
--- src/components/pages/shop_f/Shop.js
+++ src/components/pages/shop_f/Shop.js
@@ -1,6 +1,6 @@
 import Button from '../../button_f/Button';
 import styles from './Shop.module.css'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 
 const Shop = () =>
 {
@@ -8,19 +8,29 @@ const Shop = () =>
     const btn_korzina = <Button text='Корзина' add_style={styles.btn_korzina}/>;
     const btn_catalog = <Button text='...' add_style={styles.btn_catalog}/>;
 
-    
+    const [products, setProducts] = useState([]);
+
+    fetch('https://dummyjson.com/products')
+    .then(res => res.json())
+    .then(data => {
+        setProducts(data.products)
+        console.log(data.products)
+    });
+
+
 
     return(
         <div className={styles.div}>
             <div className={styles.products}>
 
 
-
-
-                 <div className={styles.product}>
-                    НАЗВАНИЕ / ЦЕНА /
+            {products.map(item => (
+                <div className={styles.product} key={item.id}>
+                    <img src={item.images} className={styles.img}/>
+                    {item.image} <p className={styles.p_product}>{item.title} | ${item.price}</p>
                     {btn_add}
                 </div>
+            ))}
 
 
             </div>
