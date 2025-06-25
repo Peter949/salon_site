a4d97c6cb997871d5e1df4aee421e037849791fd
Trofim Abramov <EmperorTerra2000@yandex.ru>
2025-06-25 21:45:24 +0900

    refact: clean code

--- src/App.js
+++ src/App.js
@@ -5,23 +5,25 @@ import Footer from './components/footer_f/Footer';
 import Body from './components/body_f/Body';
 import Catalog from './components/catalog_f/Catalog';
 import { useState } from 'react';
-import Korzina from './components/korzina_f/Korzina';
+import Korzina from './components/Korzina_f/Korzina';
 import Account from './components/pages/account_f/Account';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 
 
 const App = () => {
-  const [isAccountOpen, setAccount] = useState(false);
-  const [isCatalogOpen, setCatalog] = useState(false);
-  const [isKorzinaOpen, setKorzina] = useState(false);
-  const [korzina, set_v_korzinu] = useState([]);
+  // сам state и его метод должны быть camelCase (и лучше максимально юзать camelCase в своих проектах)
+  // также нужно чтобы стейт и метод отличались только словом в начале "set" у второго, например, [isOpen, setIsOpen]
+  // это рекомендация на будущее, не грубые ошибки
+  const [isAccountOpen, setIsAccountOpen] = useState(false);
+  const [isCatalogOpen, setIsCatalogOpen] = useState(false);
+  const [isKorzinaOpen, setIsKorzinaOpen] = useState(false);
+  const [korzina, setKorzina] = useState([]);
   const [catalogStateShop, setStateShop] = useState(false);
-  const [catalogProducts, setProducts] = useState([])
+  const [catalogProducts, setProducts] = useState([]);
 
   const updateKorzina = (value) =>
   {
-    set_v_korzinu(value);
+    setKorzina(value);
   }
   const updateProducts = (value) =>
   {
@@ -33,7 +35,7 @@ const App = () => {
   }
   const updateCatalog = (value) =>
   {
-    setCatalog(value);
+    setIsCatalogOpen(value);
   }
 
   return (
@@ -41,19 +43,19 @@ const App = () => {
       
       {isCatalogOpen && (<Catalog methods={[updateCatalog, updateShop, updateProducts]}/>
       )}
-      {isKorzinaOpen && (<Korzina korzina = {korzina} states={[isKorzinaOpen]} methods={[setKorzina, updateKorzina]}/>
+      {isKorzinaOpen && (<Korzina korzina = {korzina} states={[isKorzinaOpen]} methods={[setIsKorzinaOpen, updateKorzina]}/>
       )}
       <Header/>
-      <Navigator methods={[setAccount]}/>
+      <Navigator methods={[setIsAccountOpen]}/>
       {/* <BrowserRouter>
         <Routes>
           <Route path=''/>
           <Route path='account'/>
         </Routes>
       </BrowserRouter> */}
-      {isAccountOpen && (<Account methods={[setAccount]}/>)}
+      {isAccountOpen && (<Account methods={[setIsAccountOpen]}/>)}
       <Body states={[isCatalogOpen, catalogStateShop, catalogProducts, korzina]} 
-            methods={[updateCatalog, updateShop, setKorzina, updateKorzina]}/>
+            methods={[updateCatalog, updateShop, setIsKorzinaOpen, updateKorzina]}/>
       <Footer/>
     </div>
   );
--- src/components/navigator_f/Navigator.js
+++ src/components/navigator_f/Navigator.js
@@ -1,59 +1,44 @@
-import Button from '../button_f/Button';
-import styles from './Navigator.module.css'
-import { useState } from 'react';
+import ButtonNav from "../ButtonNav/ButtonNav";
+import styles from "./Navigator.module.css";
+import { useState } from "react";
 
-const Navigator = (props) =>
-{
-    const [setAccount] = props.methods;
+// константу можно вынести наружу
+const btn_name_arr = [
+  "Магазин",
+  "Парикмахеры",
+  "Запись на прием",
+  "Все прически",
+  "Личный кабинет",
+];
 
-    const btn_listener = (btn_index) =>
-    {
-        //switch
+const Navigator = (props) => {
+  const [setAccount] = props.methods;
+  // хуки, пропсы, переменные лучше инициализировать в начале компонента потом уже функции
+  const [btn_state, setBtnState] = useState("Магазин"); // начальным значение в данном случае нужно вставить текст а не массив
 
-        if(btn_index == 4)
-        {
-            setAccount(true);
-        }
+  const btn_listener = (btn_index) => {
+    //switch
+
+    if (btn_index == 4) {
+      setAccount(true);
     }
+  };
 
-    const btn_name_arr = ["Магазин", "Парикмахеры", "Запись на прием", "Все прически", "Личный кабинет"];
-    const [btn_state, setBtnState] = useState(["Магазин"]);
-
-    const btn_styles = [styles.btn_style, styles.btn_style, styles.btn_style, styles.btn_style, styles.btn_style];
-
-    switch(btn_state)
-    {
-        case btn_name_arr[0]:
-            btn_styles[0] = styles.active_btn_style;
-            break;
-        case btn_name_arr[1]:
-            btn_styles[1] = styles.active_btn_style;
-            break;
-        case btn_name_arr[2]:
-            btn_styles[2] = styles.active_btn_style;
-            break;
-        case btn_name_arr[3]:
-            btn_styles[3] = styles.active_btn_style;
-            break;
-        case btn_name_arr[4]:
-            btn_styles[4] = styles.active_btn_style;
-            break;
-    }
-    
-    const btn_arr = btn_name_arr.map((name, index) => (
-        <Button 
-            key={name} 
-            text={name} 
-            add_style = {btn_styles[index]}
-            onClick={() => {setBtnState(name); btn_listener(index)}}
+  return (
+    <div className={styles.div}>
+      {btn_name_arr.map((name, index) => (
+        <ButtonNav
+          key={index}
+          text={name}
+          isActive={btn_state === name}
+          onClick={() => {
+            setBtnState(name);
+            btn_listener(index);
+          }}
         />
-    ));
-
-    return(
-        <div className={styles.div}>
-            {btn_arr}
-        </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
 
 export default Navigator;
